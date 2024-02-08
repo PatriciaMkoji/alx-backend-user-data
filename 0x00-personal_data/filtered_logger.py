f3558ae3ca5f935786cdc766c575aefce573bb1c
PatriciaMkoji <keyshotrisha@gmail.com>
2024-02-08 17:42:08 +0300

    pystyle code correction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
-""" Returns the log message obfuscated """
-
+""" Regex-ing and Obfuscating info """
 import re
-import logging
 
 
 def filter_datum(fields, redaction, message, separator):
-    """ function uses a regex to replace occurrences of certain field values
-        Args:
-            fields: a list of strings representing all fields to obfuscate
-            redaction: string representing by what the field will be obfuscated
-            message: a string representing the log line
-            separator: string representing by which character is separating
-    """
-    return re.sub(f'({re.escape(fields[0])}={re.escape(separator)}[^{re.escape(separator)}]+|'
-            f'{re.escape(fields[1])}={re.escape(separator)}[^{re.escape(separator)}]+)', 
-            f'{fields[0]}={redaction}{separator}{fields[1]}={redaction}{separator}', message)
+    """Obfuscates certain field values in a log message."""
+    pattern = (
+            f'({re.escape(fields[0])}={re.escape(separator)}[^{re.escape(separator)}]+|'
+            f'{re.escape(fields[1])}={re.escape(separator)}[^{re.escape(separator)}]+)'
+            )
+    replacement = f'{fields[0]}={redaction}{separator}{fields[1]}={redaction}{separator}'
+    return re.sub(pattern, replacement, message)
