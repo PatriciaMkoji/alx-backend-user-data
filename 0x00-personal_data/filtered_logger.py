eb86e00dc507cf04b3fe11d7e8d5f52e467513c3
PatriciaMkoji <keyshotrisha@gmail.com>
2024-02-08 18:59:26 +0300

    Log Formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 """ Regex-ing and Obfuscating info """
 import re
+import logging
 
 
-def filter_datum(fields, redaction, message, separator):
-    """Obfuscates certain field values in a log message."""
-    pattern = (
-            f'({re.escape(fields[0])}={re.escape(separator)}[^{re.escape(separator)}]+|'
-            f'{re.escape(fields[1])}={re.escape(separator)}[^{re.escape(separator)}]+)'
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+                """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super(RedactingFormatter, self).format(record)
+        return self.filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+    
+    @staticmethod
+    def filter_datum(fields, redaction, message, separator):
+        """Obfuscates certain field values in a log message."""
+        pattern = (
+                f'({re.escape(fields[0])}={re.escape(separator)}[^{re.escape(separator)}]+|'
+                f'{re.escape(fields[1])}={re.escape(separator)}[^{re.escape(separator)}]+)'
             )
-    replacement = f'{fields[0]}={redaction}{separator}{fields[1]}={redaction}{separator}'
-    return re.sub(pattern, replacement, message)
+        replacement = f'{fields[0]}={redaction}{separator}{fields[1]}={redaction}{separator}'
+        return re.sub(pattern, replacement, message)
