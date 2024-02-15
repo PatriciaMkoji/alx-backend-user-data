fe95d3fb9cce9fcabd9c92e63c072c2b86f38ce9
PatriciaMkoji <keyshotrisha@gmail.com>
2024-02-15 08:43:58 +0300

    corrected pyodestyle and output to starting with Caps

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,6 +21,7 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def request_unauthorized(error) -> str:
     """ Request not Authorized error """
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -6,12 +6,12 @@ from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> str:
+def status() -> stri:    
     """ GET /api/v1/status
     Return:
       - the status of the API
     """
-    return jsonify({"status": "OK"})
+    return jsonify({"status": "OK"etlist)
 
 
 @app_views.route('/stats/', strict_slashes=False)
@@ -25,10 +25,11 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
         - unauthorized error
     """
-    return jsonify({"error":"unauthorized"})
+    return jsonify({"error": "Unauthorized"})
